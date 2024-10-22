191b82620c987f3ed318e02d916acba6a383d0c7
calvin.cliff <calvin.cliff@impact-multimedia.com>
2024-10-22 11:44:24 +0800

    feat: adds created components as sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
   import Hero from "$lib/components/Hero.svelte";
+  import Product from "$lib/components/Product.svelte";
+  import Reviews from "$lib/components/Reviews.svelte";
 </script>
 
 <main class="flex flex-col">
   <Hero />
+  <Product />
+  <Reviews />
 </main>
