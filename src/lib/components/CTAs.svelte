ca91f0c7053a0ca5ba7626d87a3f150ac67de0fb
calvin.cliff <calvin.cliff@impact-multimedia.com>
2024-10-22 15:03:16 +0800

    feat: adds a prop dark into the CTA component

--- src/lib/components/CTAs.svelte
+++ src/lib/components/CTAs.svelte
@@ -1,8 +1,10 @@
-<script></script>
+<script lang="ts">
+  export let dark: boolean = false;
+</script>
 
 <div class="flex items-center gap-4">
-  <button class="specialBtn">
-    <p class="text-base sm:text-lg md:text-xl">Us</p>
+  <button class={"specialBtn " + (dark ? "bg-[#181b34]" : " ")}>
+    <p class="text-base sm:text-lg md:text-xl">ContactUs</p>
   </button>
   <button class="specialBtnDark">
     <p class="text-base sm:text-lg md:text-xl">Get Started &rarr;</p>
