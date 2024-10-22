7cf08a9f89bca90e2df74263cfe2502700d6a6ba
calvin.cliff <calvin.cliff@impact-multimedia.com>
2024-10-22 15:05:12 +0800

    feat: creates the Star component. Moves the star codes into it.

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -2,6 +2,7 @@
   import SectionWrapper from "$lib/components/SectionWrapper.svelte";
   import Header from "$lib/components/Header.svelte";
   import CtAs from "./CTAs.svelte";
+  import Stars from "./Stars.svelte";
 </script>
 
 <SectionWrapper id="hero">
@@ -27,20 +28,6 @@
       and much much more.
     </p>
     <CtAs />
-    <div class="flex items-center justify-center gap-2 text-base">
-      <p>4.6</p>
-      {#each [0, 1, 2, 3, 4] as index}
-        <div class="grid place-items-center relative">
-          <i class="fa-solid fa-star opacity-0"></i>
-          <div
-            class={"absolute top-0 left-0 grid place-items-center " +
-              (index === 4 ? "w-[60%] overflow-hidden" : "")}
-          >
-            <i class="fa-solid fa-star text-amber-400"></i>
-          </div>
-        </div>
-      {/each}
-      <p>500+</p>
-    </div>
+    <Stars />
   </div>
 </SectionWrapper>
