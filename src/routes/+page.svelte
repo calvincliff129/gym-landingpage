fd3246604724b636957800a633ea5f080c72cdcb
calvin.cliff <calvin.cliff@impact-multimedia.com>
2024-10-22 15:07:38 +0800

    feat: adds in the components - FAQs and Conversion into the landing page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,14 @@
   import Hero from "$lib/components/Hero.svelte";
   import Product from "$lib/components/Product.svelte";
   import Reviews from "$lib/components/Reviews.svelte";
+  import FAQs from "$lib/components/FAQs.svelte";
+  import Conversion from "$lib/components/Conversion.svelte";
 </script>
 
 <main class="flex flex-col">
   <Hero />
   <Product />
   <Reviews />
+  <FAQs />
+  <Conversion />
 </main>
